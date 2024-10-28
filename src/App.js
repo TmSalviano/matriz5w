dce6489ba737ea968adacc9c9cb3e4682403f54b
TmSalviano <tiagomsalviano@outlook.com>
2024-10-28 14:40:12 -0300

    Add: Initial Layout

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import "./styles/App.css";
 function TextInput() {
 
   return (
-    <div className='container'>
-      <Card />
-      <Card />
+    <div>
+      <header className='container' style={{border: '3px solid white'}}>
+        <h1>Matriz Egide</h1>
+      </header>
+      <main className='container'>
+        <Card />
+        <Card />
+        <Card /> 
+        <Card />
+      </main>
     </div>
   );
 }
